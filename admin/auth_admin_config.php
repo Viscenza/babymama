3069af8536260506c202b59427b6146ecbadc975
Dar-rius <mohamedtine17@gmail.com>
2022-05-22 23:22:50 +0000

    update: admin front and back

---
+++ admin/auth_admin_config.php
@@ -0,0 +1,34 @@
+<!-- cote serveur de la page auth-->
+
+<?php
+    /* importation du fichier de connexion vers la base de
+     Donnee */
+        include '../config.php';
+
+session_start();
+
+
+    /* verification si le nom et password sont hesiste dans le BD */
+        $query = "SELECT * FROM admins WHERE user_name = :user_name AND password = :password";  
+        $statement = $bd->prepare($query);  
+        $statement->execute(  
+                array(  
+                'user_name'=> $_POST["user_name"],  
+                'password'=>$_POST["password"]  
+                )  
+        );  
+
+        $count = $statement->rowCount();  
+        
+        if($count > 0)  
+        {  
+                $_SESSION["user_name"] = $_POST["user_name"];
+                echo '<script>
+                    location.href="./admin.php";
+                </script>';
+        }  
+        else  
+        {  
+                echo 'Error';
+        }  
+   
